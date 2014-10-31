0b2756eaf94120f2a37c7703b6da983deba913b5
lnherrer <lnsherrer@gmail.com>
2014-10-31 15:38:38 -0500

    new edits

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('header.php'); ?>
+<?php include('includes/header.php'); ?>
 
 <?php
 if ($login->isUserLoggedIn() == true) 
@@ -30,4 +30,4 @@ else {
 }
 ?>
 
-<?php include('footer.php'); ?>
+<?php include('includes/footer.php'); ?>
