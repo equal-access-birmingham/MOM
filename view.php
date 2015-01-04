b53930fc0bb0ba1310f217322727affbc5e9a558
tikenn <tikenn7792@gmail.com>
2015-01-04 15:18:04 -0600

    Update view.php

--- view.php
+++ view.php
@@ -1,48 +1,43 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+$require_login = true;
 
-require_once("includes/header-require_login.php");
-require_once("includes/menu_sign_in.php");
+$path_to_root = str_repeat("../", substr_count($_SERVER['SCRIPT_NAME'], "/") - 1);
+require_once($path_to_root . "includes/header.php");
+?>
+
+    <title>My Schedule | Equal Access Birmingham</title>
+
+<?php require_once($path_to_root . "includes/menu.php");
 
 // Sets up the database connection
 try
 {
 	$con = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 }
-
 catch(PDOException $e)
 {
 	echo 'Connection failed: ' . $e->getMessage();
 }
-
 	echo "
-		<a href=\"index.php\">" . WORDING_HOME_PAGE . "</a>
-		<a href=\"view.php\">" . WORDING_DATA_VIEW . "</a>
-		<a href=\"entry_test3.php\">" . WORDING_EVENT_REGISTRATION . "</a>
-		<a href=\"index.php?logout\">" . WORDING_LOGOUT . "</a>
-    ";
-
+    <a href=\"index.php\">" . WORDING_HOME_PAGE . "</a>
+    <a href=\"view.php\">" . WORDING_DATA_VIEW . "</a>
+    <a href=\"entry_test3.php\">" . WORDING_EVENT_REGISTRATION . "</a>
+    <a href=\"index.php?logout\">" . WORDING_LOGOUT . "</a>
+	";
 // user_id from the Login() class
 $user_id = $_SESSION['user_id'];
 
-
 // MySQL query for full name of user based on $user_id
-$query=
-"SELECT person_table.fname, person_table.lname, person_table.suffname
-	FROM `person_table`
-	JOIN `login_relation_table`
-	ON person_table.person_id = login_relation_table.person_id
-	WHERE login_relation_table.user_id = :user_id"; 
+$query = "SELECT person_table.fname, person_table.lname, person_table.suffname
+		FROM `person_table`
+			JOIN `login_relation_table`
+			ON person_table.person_id = login_relation_table.person_id
+			WHERE login_relation_table.user_id = :user_id";
+
 $stmt_user_full_name = $con->prepare ($query);
 $stmt_user_full_name->bindValue (':user_id', $user_id, PDO::PARAM_STR);
 $stmt_user_full_name->execute ();
 $result = $stmt_user_full_name->fetch();
-
-echo "
-  <h1>Hi, Welcome " . $result['fname'] . " " . $result['lname'] . " " . $result['suffname'] . "</h1>";
-echo "
-  <h2>Volunteer Schedule</h2>";
 
 // MySQL query for user's schedule
 // Captures all dates the user is signed up for
@@ -56,14 +51,14 @@ $query = "SELECT temp4.role_name, temp4.program_name, temp4.date, temp4.arrival_
 							FROM (
 								SELECT program_relation_table.program_id, program_relation_table.date, program_relation_table.location_id, temp.role_id
 									FROM (
-										SELECT signup_table.program_id, signup_table.role_id
+										SELECT signup_table.program_relation_id, signup_table.role_id
 											FROM `signup_table`
 											JOIN `login_relation_table`
 											ON signup_table.login_relation_id = login_relation_table.login_relation_id
 											WHERE login_relation_table.user_id = :user_id
 									) AS temp
 									JOIN program_relation_table
-									ON temp.program_id = program_relation_table.program_id
+									ON temp.program_relation_id = program_relation_table.program_id
 							) AS temp1
 							JOIN program_table
 							ON temp1.program_id = program_table.program_id
@@ -76,13 +71,18 @@ $query = "SELECT temp4.role_name, temp4.program_name, temp4.date, temp4.arrival_
 	)AS temp4
 	JOIN location_table
 	ON temp4.location_id = location_table.location_id";
-
 $stmt_user_schedule = $con->prepare ($query);
 $stmt_user_schedule->bindValue (':user_id', $user_id, PDO::PARAM_STR);
-$stmt_user_schedule->execute ();
+$stmt_user_schedule->execute();
+?>
+    <div class="container no-image">
 
-$event_count = 0;
+      <?php echo "<h1>Hi, Welcome " . $result['fname'] . " " . $result['lname'] . " " . $result['suffname'] . "</h1>"; ?>
 
+      <h2>Volunteer Schedule</h2>
+
+<?php
+$event_count = 1;
 while($result = $stmt_user_schedule->fetch())
 {
 	/**
@@ -94,16 +94,17 @@ while($result = $stmt_user_schedule->fetch())
 	$time = new DateTime($result['arrival_time']);
 	
 	echo "
-        <h3>Volunteer Time $i</h3>
-          <ul>
-            <li>Role: " . $result['role_name']. "</li>
-            <li>Program: " . $result['program_name'] . "</li>
-            <li>Date: " . $date->format("F j, Y") . "</li>
-            <li>Arrival Time: " . $time->format("g:i A") . "</li>
-            <li>Address: " . $result['address'] . "</li> 
-          </ul>
-          ";
-
+      <h3>Volunteer Time $event_count</h3>
+      <ul>
+        <li>Role: " . $result['role_name']. "</li>
+        <li>Program: " . $result['program_name'] . "</li>
+        <li>Date: " . $date->format("F j, Y") . "</li>
+        <li>Arrival Time: " . $time->format("g:i A") . "</li>
+        <li>Address: " . $result['address'] . "</li> 
+      </ul>\n";
 	$event_count++;
 }
 ?>
+    </div>
+
+<?php require_once($path_to_root . "includes/footer.php"); ?>
